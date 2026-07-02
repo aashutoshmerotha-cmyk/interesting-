9f23ad60685bf99d4d69ddff5ceafa165771ca4b
aashutoshmerotha-cmyk <aashutoshmerotha@gmail.com>
2026-07-02 15:11:21 +0530

    Initial upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "interesting-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  // Network-first for everything (so Firestore data & app updates stay fresh),
+  // falling back to cache only when offline.
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        if(event.request.method === "GET" && res.ok){
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
